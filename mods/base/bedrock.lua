68d96c26d7fdc9dd809a6c27b239a59a7dede15e
Jozef Behran <jozuejozef@gmail.com>
2019-02-13 02:20:22 -0500

    Bedrock layer at the bottom of the world
    Add bedrock layer filling the last four mapchunk heights
    (320 blocks) with bedrock. This sets that area apart to be
    used for various purposes internal to the code that forms the
    fabric of the world.

---
+++ mods/base/bedrock.lua
@@ -0,0 +1,38 @@
+-- The bottom of the world is covered with a bedrock layer which cannot be
+-- destroyed by any means.
+
+local layer = -30593
+
+minetest.register_node("base:bedrock", {
+  tiles = {"base_bedrock.png"},
+  groups = {immortal=1, not_in_creative_inventory=1, },
+  sounds = { footstep = { name = "bedrock_step", gain = 1 } },
+  is_ground_content = false,
+  on_blast = function() end,
+  on_destruct = function () end,
+  can_dig = function() return false end,
+  diggable = false,
+})
+
+minetest.register_on_generated(function(minp, maxp)
+  if minp.y <= layer then
+    local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+    local data = vm:get_data()
+    local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
+    local c_bedrock = minetest.get_content_id("base:bedrock")
+
+    for x = minp.x, maxp.x do
+      for y = minp.y, maxp.y do
+        for z = minp.z, maxp.z do
+          local p_pos = area:index(x, y, z)
+          data[p_pos] = c_bedrock
+        end
+      end
+    end
+
+    vm:set_data(data)
+    vm:calc_lighting()
+    vm:update_liquids()
+    vm:write_to_map()
+  end
+end)
