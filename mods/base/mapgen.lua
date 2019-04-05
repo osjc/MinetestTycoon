ef0c63b229a3971c382a3d16f885daee2474c822
Jozef Behran <jozuejozef@gmail.com>
2019-04-05 01:21:02 -0500

    Putting time deltas for mapgen into mapgen log
    The mapgen log can now contain "Tnnn" records between the
    specifier and the location (if any). This specifies how much
    time passed since the last line. If there is no such
    specifier on the line, the time passed is 0 otherwise it is
    1 more than the value after the "T". The time is in
    deciseconds.

--- mods/base/mapgen.lua
+++ mods/base/mapgen.lua
@@ -12,6 +12,18 @@ local function PackChunkIndex(pos, cy, cz)
   return ChunkIndex
 end
 
+LastTime=0
+
+local function GetTimeReport()
+  local TimeReport=""
+  local CurrentTime = clock.GetTime()
+  if CurrentTime>LastTime then
+    TimeReport=" T"..(CurrentTime-LastTime-1)
+    LastTime=CurrentTime
+  end
+  return TimeReport
+end
+
 LogLineNum=1
 ReportedBlocks={}
 
@@ -35,10 +47,11 @@ minetest.register_on_generated(function(minp, maxp)
   local MaxpStr = PosToString(maxp)
   local PosReport = " "..MinpStr.." "..MaxpStr
   local Previous=ReportedBlocks[Index]
+  local TimeReport=GetTimeReport()
   if Previous then
     local Msg="Duplicated emerge: "..Index
     print(Msg.." first seen at line "..Previous.Line)
-    local Report=":"..(LogLineNum-1-Previous.Line)
+    local Report=":"..(LogLineNum-1-Previous.Line)..TimeReport
     if Previous.Report~=PosReport then
       Report=Report..PosReport
     end
@@ -46,19 +59,46 @@ minetest.register_on_generated(function(minp, maxp)
     return
   end
   ReportedBlocks[Index]={Line=LogLineNum,Report=PosReport}
-  print("Emerge: "..Index)
-  PutToLog(Index..PosReport)
+  print("Emerge: "..Index.." "..LastTime)
+  PutToLog(Index..TimeReport..PosReport)
 end)
 
 local function LoadLog()
   util.IterateOverLines("mapgen.log", function(Line)
-    LogLineNum=LogLineNum+1
+    local FirstChar=string.sub(Line,1,1)
     local FirstPos=string.find(Line," ")
-    if FirstPos ~= nil then
-      local IndexStr=string.sub(Line, 1, FirstPos-1)
-      local Index=tonumber(IndexStr)
-      local PosReport=string.sub(Line,FirstPos,-1)
+    local IndexEnd=FirstPos-1
+    local PosReport=nil
+    while true do
+      local StartPos=FirstPos+1
+      local NextItem=string.sub(Line,StartPos,StartPos)
+      if NextItem=="[" then
+        PosReport=string.sub(Line,FirstPos,-1)
+        break
+      end
+      FirstPos=string.find(Line," ",StartPos)
+      if NextItem=="T" then
+        local EndPos=FirstPos
+        if EndPos==nil then
+          EndPos=-1
+        else
+          EndPos=EndPos-1
+        end
+        StartPos=StartPos+1
+        local TimeDeltaStr=string.sub(Line,StartPos,EndPos)
+        local TimeDelta=tonumber(TimeDeltaStr)+1
+        LastTime=LastTime+TimeDelta
+      end
+      if FirstPos==nil then
+        break
+      end
     end
+    if FirstChar>="0" and FirstChar<="9" and FirstPos ~= nil then
+      local IndexStr=string.sub(Line, 1, IndexEnd)
+      local Index=tonumber(IndexStr)
+      ReportedBlocks[Index]={Line=LogLineNum,Report=PosReport}
+    end
+    LogLineNum=LogLineNum+1
   end)
 end
 
